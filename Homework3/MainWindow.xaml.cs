0a3633fc9e5fe2943c64af519620dee9cbe4f4ed
cdav929 <chriswdavenport@hotmail.com>
2021-05-13 16:15:30 -0700

    Added Homework3

---
+++ Homework3/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿//UWNetID: chrisd94
+//Name: Christopher Davenport
+//Class: CSHP320 A sp 21: Creating Client Applications using .Net Core
+//Homework 3
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+
+namespace Homework3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+            uxList.ItemsSource = users;
+           
+        }
+
+		private void lvUsersColumnHeader_Click(object sender, RoutedEventArgs e)
+		{
+			GridViewColumnHeader column = (sender as GridViewColumnHeader);
+			string sortBy = column.Tag.ToString();
+			if (listViewSortCol != null)
+			{
+				AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+				uxList.Items.SortDescriptions.Clear();
+			}
+
+			ListSortDirection newDir = ListSortDirection.Ascending;
+			if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+				newDir = ListSortDirection.Descending;
+
+			listViewSortCol = column;
+			listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+			AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+			uxList.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+		}
+	}
+
+	public enum SexType { Male, Female };
+
+	public class User
+	{
+		public string Name { get; set; }
+
+		public int Password { get; set; }
+
+		
+	}
+
+	public class SortAdorner : Adorner
+	{
+		private static Geometry ascGeometry =
+			Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
+
+		private static Geometry descGeometry =
+			Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+		public ListSortDirection Direction { get; private set; }
+
+		public SortAdorner(UIElement element, ListSortDirection dir)
+			: base(element)
+		{
+			this.Direction = dir;
+		}
+
+		protected override void OnRender(DrawingContext drawingContext)
+		{
+			base.OnRender(drawingContext);
+
+			if (AdornedElement.RenderSize.Width < 20)
+				return;
+
+			TranslateTransform transform = new TranslateTransform
+				(
+					AdornedElement.RenderSize.Width - 15,
+					(AdornedElement.RenderSize.Height - 5) / 2
+				);
+			drawingContext.PushTransform(transform);
+
+			Geometry geometry = ascGeometry;
+			if (this.Direction == ListSortDirection.Descending)
+				geometry = descGeometry;
+			drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+
+			drawingContext.Pop();
+		}
+	}
+}
+        
+    
+
+

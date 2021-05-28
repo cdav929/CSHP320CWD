86d033c6f001aeb4572053576b1d6b4abffcf111
cdav929 <chriswdavenport@hotmail.com>
2021-05-27 18:51:26 -0700

    Added Homework 5

---
+++ Homework_5/DisplayIcon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_5
+{
+    public enum DisplayIcon
+    {
+        Empty,
+        Zero,
+        Cross
+
+    }
+}

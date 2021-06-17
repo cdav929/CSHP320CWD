b49ff2021b77a134c0b57375cb0624fed0823bd7
cdav929 <chriswdavenport@hotmail.com>
2021-06-17 15:29:21 -0700

    Added BirthdayCardGen

---
+++ BirthdayCardGen/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using BirthdayCardGen.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BirthdayCardGen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View(new GuestResponse());
+        }
+        [HttpPost]
+        public IActionResult RsvpForm(Models.GuestResponse guestResponse)
+
+        {
+            if (ModelState.IsValid)
+            {
+                return View("NewCard", guestResponse);
+            }
+            else
+            {
+                return View(guestResponse);
+            }
+            //{
+            //    return View("NewCard", guestResponse);
+            //}
+        }
+    }
+}
